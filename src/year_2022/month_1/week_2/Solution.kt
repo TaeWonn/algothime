efafea04a2669af3531a972a7edee4934065bc1d
TaeWonn <xodns9485@gmail.com>
2022-01-16 13:05:52 +0900

    Solution

--- src/year_2022/month_1/week_2/Solution.kt
+++ src/year_2022/month_1/week_2/Solution.kt
@@ -1,27 +1,46 @@
 package year_2022.month_1.week_2
 
 class Solution {
-
+    lateinit var list: List<Int>
     fun solution(clothes: Array<Array<String>>): Int {
-        var answer = 0
-        var idx = 0
-        val map = HashMap<String, MutableList<String>>()
-        clothes.map { it[1] to it[0] }.forEach { (fir, sec) ->
-            takeUnless { map[fir]?.add(sec) != null }?.run { map[fir] = mutableListOf(sec) }
+        var map: MutableMap<String, Int> = mutableMapOf()
+        clothes.forEach {
+            map[it[1]] = when {
+                map.containsKey(it[1]) -> map[it[1]]!!.plus(1)
+                else -> 1
+            }
         }
 
-        //경우의 수
-        val list = map.values.toList()
-        list.forEachIndexed { index, mutableList ->
+        list = map.values.toList()
 
+        var answer = 0
+        for (i in 1..map.size) {
+            answer += calc(i, map.size, 0)
         }
 
         return answer
     }
 
-    fun dfs(idx: Int, list: List<MutableList<String>>): Int {
-        return 0
+    fun calc(idx: Int, depth: Int, count: Int): Int {
+        if (idx == 0) return 1
+        if (count == depth) return 0
+        if (idx > 0 && count >= depth) return 0
+
+        var sum = 0
+        for (i in 0 until list[count])
+            sum += calc(idx -1, depth, count + 1)
+
+        if (idx <= depth - count + 1)
+            sum += calc(idx, depth, count + 1)
+
+        return sum
     }
+
+//    fun solution(clothes: Array<Array<String>>): Int {
+//        return clothes.groupBy { it[1] }.values.fold(1) {
+//                total, next -> total + (next.size + 1)
+//        } - 1
+//    }
 }
 
 fun main() {
