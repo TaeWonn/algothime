3fb15cb34d1f80fe4b145c47a8ba4be02ffa1bde
TaeWonn <xodns9485@gmail.com>
2022-05-31 23:28:10 +0900

    Solution

---
+++ src/year_2022/month_5/week_5/Solution.kt
@@ -0,0 +1,24 @@
+package year_2022.month_5.week_5
+
+class Solution {
+    fun largestWordCount(messages: Array<String>, senders: Array<String>): String {
+        val hashMap = HashMap<String, Int>()
+
+        messages.forEachIndexed { i, s ->
+            val count = hashMap.getOrDefault(senders[i], 0)
+            hashMap[senders[i]] = s.split(" ").size + count
+        }
+        val comparator = compareByDescending<MutableMap.MutableEntry<String, Int>> { it.value }.thenByDescending { it.key }
+
+        return hashMap.entries
+            .sortedWith(comparator)
+            .first()
+            .key
+    }
+}
+
+fun main() {
+    val s = Solution()
+
+    println(s.largestWordCount(arrayOf("Hello userTwooo","Hi userThree","Wonderful day Alice","Nice day userThree"), arrayOf("Alice","userTwo","userThree","Alice")))
+}
